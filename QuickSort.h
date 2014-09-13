71b0a31ee55591f03a8c5f977573f61f87014c34
xsun89 <sunxin89@gmail.com>
2014-09-13 11:41:52 -0700

    add

--- QuickSort.h
+++ QuickSort.h
@@ -5,7 +5,8 @@ using namespace std;
 template<class T>
 void QuickSort(T *a, const int left, const int right) {
 
-
+        if(left > right)
+            return;
         //选枢轴
         int i = left;
         int j = right+1; //为什么要加一?
@@ -13,8 +14,8 @@ void QuickSort(T *a, const int left, const int right) {
 
         //划分算法
         do{
-            do i++; while(a[i]<pivot && i<j);
             do j--; while(a[j]>pivot && i<j);
+            do i++; while(a[i]<pivot && i<j);
             if(i<j) swap(a[i],a[j]);
         }while(i<j);
         swap(a[left],a[j]);
